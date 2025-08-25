a6a62f0bed980fc5d19f43e776b419d13c208f37
ChaudhariSnehal1706 <chaudharis6009@gmail.com>
2025-08-25 17:12:51 +0530

    update StatChartCard

--- src/components/commonComponent/StatChartCard.jsx
+++ src/components/commonComponent/StatChartCard.jsx
@@ -25,17 +25,17 @@ const CustomTooltip = ({ active, payload }) => {
   return null;
 };
 
-const StatChartCard = ({ title, value, change, trend, color, chartColor = "#FF542A" }) => {
+const StatChartCard = ({ title, value, change, trend, color, chartColor = "#675DFF" }) => {
   const { language } = useLanguage();
   const isRTL = language === 'he';
   const gradientId = React.useId();
 
   return (
     <div className="bg-white dark:bg-customBrown rounded-xl p-[20px] border border-gray-200 dark:border-commonBorder  relative dark:hover:bg-customBlack hover:bg-customBody shadow-md hover:shadow-sm">
-      <span className="text-gray-900 dark:text-white text-14 mb-2">{title}</span>
+      <span className="text-gray-900 dark:text-white text-14">{title}</span>
       <div className="flex items-end justify-between">
         <div className="flex flex-col">
-          <span className="text-44 font-semibold text-gray-900 dark:text-white my-2 font-ttcommons">{value}</span>
+          <span className="text-44 font-semibold text-gray-900 dark:text-white font-ttcommons">{value}</span>
           <div className={`flex items-center ${isRTL ? 'flex-row-reverse' : ''} gap-2`}>
             <span className={`text-12 ${color === 'green' ? 'text-customGreen' : 'text-customRed'}`}>
               {change > 0 ? '+' : ''}{change}%
