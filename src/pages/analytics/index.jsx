1859c6c65b5fbbce04a70f3c3a13b8a5de083b0c
Plusfive <aviel9552@gmail.com>
2025-11-05 18:52:26 +0200

    Update index.jsx

--- src/pages/analytics/index.jsx
+++ src/pages/analytics/index.jsx
@@ -13,11 +13,11 @@ function Analytics() {
       <AdminAnalyticsRevenueAndCustomerStatus />
       <AdminAnalyticsSecontChart />
       <AdminLTVGrothChart />
+          <div className='mt-10' />...>
        <AdminAnalyticsMonthlyPerformance />
       {/* <div className='flex flex-col gap-[16px]'>
         <h2 className='text-[24px] font-bold mt-10 dark:text-white'>{t.analytics}</h2>
       </div> */}
-      <div className='mt-10' />...>
     </div>
   );
 }
