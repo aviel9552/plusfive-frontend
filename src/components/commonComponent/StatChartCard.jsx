b9dfc3e87f368a78c016849f56c0751522be7778
Plusfive <aviel9552@gmail.com>
2025-12-18 02:48:34 +0200

    Update StatChartCard.jsx

--- src/components/commonComponent/StatChartCard.jsx
+++ src/components/commonComponent/StatChartCard.jsx
@@ -34,7 +34,7 @@ const StatChartCard = ({ title, value, change, trend, color, chartColor = "#ff25
     <div className="bg-white dark:bg-customBrown rounded-xl p-[20px] border border-gray-200 dark:border-commonBorder relative dark:hover:bg-customBlack hover:bg-customBody shadow-md hover:shadow-sm">
      <span
   style={isRTL ? { fontFamily: 'Nunito Sans' } : undefined}
-  className={`text-14 text-black dark:text-white ${isRTL ? 'font-extrabold' : 'font-medium'}`}
+  className={`text-18 text-black dark:text-white ${isRTL ? 'font-regular' : 'font-medium'}`}
 >
   {title}
 </span>
