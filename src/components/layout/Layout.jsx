502713e900bd9951d31297856a5a8dd37e645df9
Plusfive <aviel9552@gmail.com>
2025-12-24 02:22:58 +0200

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
       />
       <div className={`flex flex-col flex-1 transition-all duration-300 min-w-0 ${isMobile ? (isRTL ? 'mr-0' : 'ml-0') : (isDesktopSidebarCollapsed ? (isRTL ? 'lg:mr-16' : 'lg:ml-16') : (isRTL ? 'lg:mr-[288px]' : 'lg:ml-[288px]'))}`}>
         <Header onMobileMenuToggle={() => setIsMobileMenuOpen(true)} />
-        <main className="flex-1 overflow-y-auto p-6 pt-[72px] lg:pt-[66px]">
+        <main className="flex-1 overflow-y-auto p-6 pt-[96px] lg:pt-[88px]">
   {children}
 </main>
         {/* <Footer /> */}
