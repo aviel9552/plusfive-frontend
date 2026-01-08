7835c43fd48cb9002532698c6d6c149118f55bb9
Aviel <aviel9552@gmail.com>
2026-01-08 13:57:11 +0200

    test aviel 123

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from '../lib/store'
+
+
+
+
+
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor } from '../lib/store'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+//this code added by aviel
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
